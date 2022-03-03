04bddba2c795fce5cb101d3ff4180c86b23d8b8d
Kazuya Hara <iam@kazuyahara.dev>
2022-03-03 22:34:06 +0900

    chore: use eslint

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import React from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: { alignItems: 'center', backgroundColor: '#fff', flex: 1, justifyContent: 'center' },
+});
 
 export default function App() {
   return (
@@ -9,12 +15,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
